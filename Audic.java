6cf6282680b3a8b4fe16d3e07f0c057edf6e7520
Cheang Jia Jun <61217047+jiajunc09@users.noreply.github.com>
2020-05-30 11:31:30 +0800

    Create Audic.java

---
+++ Audic.java
@@ -0,0 +1,33 @@
+public class Audic extends Car{
+    public static void audi(){
+        System.out.print("Select Car to Review The Specification" + "\n" + "1)Audi A1" + "\n" + "2)Audi A3 Sedan" + "\n" + "3)Audi Q2 sport" + "\n" + "4)Audi Q3" + "\n" + "5)Back" + "\n" + "Select: ");
+    }
+    public static void audia1(){
+        System.out.println("Audi A1 1.4 TFSI (2014)");
+        System.out.println("Engine Tech: 16-valve DOHC, direct fuel injection, turbocharged with variable valve timing");
+        System.out.println("Capacity: 1390 cc");
+        System.out.println("Power: 122 hp at 5,000 rpm");
+        System.out.println("Bore x Stroke: 76.5mm x 75.6mm");
+    }
+    public static void audia3(){
+        System.out.println("Audi A3 Sedan 1.4 TFSI (2017)");
+        System.out.println("Engine Tech: 16-valve DOHC, direct fuel injection, turbocharged with variable valve timing");
+        System.out.println("Capacity: 1395cc");
+        System.out.println("Power: 122 hp at 5,000 rpm");
+        System.out.println("Bore x Stroke: 76.5mm x 76.5mm");
+    }
+    public static void audiq2(){
+        System.out.println("Audi Q2 sport 1.4 TFSI (2019)");
+        System.out.println("Engine Tech: 16-valve DOHC, direct fuel injection, turbocharged with variable valve timing");
+        System.out.println("Capacity: 1395 cc");
+        System.out.println("Power: 150 hp at 5,000 rpm");
+        System.out.println("Bore x Stroke: 74.5mm x 80mm");
+    }
+    public static void audiq3(){
+        System.out.println("Audi Q3 1.4 TFSI (2019)");
+        System.out.println("Engine Tech: 16-valve DOHC, direct fuel injection, turbocharged with variable valve timing");
+        System.out.println("Capacity: 1395 cc");
+        System.out.println("Power: 147 hp at 5,000 rpm");
+        System.out.println("Bore x Stroke: 74.5mm x 80mm");
+    }
+}
