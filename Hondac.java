ed5b328aa4313e2f9c4cd47b6dd56c5554c5cbf4
Cheang Jia Jun <61217047+jiajunc09@users.noreply.github.com>
2020-05-30 11:36:19 +0800

    Create Hondac.java

---
+++ Hondac.java
@@ -0,0 +1,33 @@
+public class Hondac extends Car{
+    public static void honda(){
+        System.out.println("Select Car to Review The Specification" + "\n" + "1)Honda HR-V" + "\n" + "2)Honda Civic Type R" + "\n" + "3)Honda CR-Z" + "\n" + "4)Honda City" + "\n" + "5)Back" + "\n" + "Select: ");
+    }
+    public static void hondahr(){
+        System.out.println("Honda HR-V 1.8 CVT S (2018)");
+        System.out.println("Engine Tech: 16-valve SOHC, naturally-aspirated with variable valve timing");
+        System.out.println("Capacity: 1799 cc");
+        System.out.println("Power: 140 hp at 6,500 rpm");
+        System.out.println("Bore x Stroke: 81mm x 87.3mm");
+    }
+    public static void hondacv(){
+        System.out.println("Honda Civic Type R (2018)");
+        System.out.println("Engine Tech: 16-valve DOHC, turbocharged with variable valve timing");
+        System.out.println("Capacity: 1996 cc");
+        System.out.println("Power: 306 hp at 6,500 rpm");
+        System.out.println("Bore x Stroke: 86mm x 85.9mm");
+    }
+    public static void hondacrz(){
+        System.out.println("Honda CR-Z Hybrid 1.5 MT (2015)");
+        System.out.println("Engine Tech: 16 valve SOHC, naturally-aspirated with variable valve timing and electric motor assist");
+        System.out.println("Capacity: 1496 cc");
+        System.out.println("Power: 118 hp at 6,600 rpm");
+        System.out.println("Bore x Stroke: 73mm x 89.4mm");
+    }
+    public static void hondac(){
+        System.out.println("Honda City 1.5 S (2020)");
+        System.out.println("Engine Tech: 16 valve SOHC, naturally-aspirated with variable valve timing");
+        System.out.println("Capacity: 1497 cc");
+        System.out.println("Power: 118 hp at 6,600 rpm");
+        System.out.println("Bore x Stroke: 73mm x 89.4mm");
+    }
+}
