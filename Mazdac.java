36d9639dc967e2a777972480179ab58ba56423b5
Cheang Jia Jun <61217047+jiajunc09@users.noreply.github.com>
2020-05-30 11:36:56 +0800

    Create Mazdac.java

---
+++ Mazdac.java
@@ -0,0 +1,33 @@
+public class Mazdac extends Car{
+    public static void mazda(){
+        System.out.println("Select Car to Review The Specification" + "\n" + "1)Mazda Biante" + "\n" + "2)Mazda CX-9" + "\n" + "3)Mazda BT-50" + "\n" + "4)Mazda CX-30" + "\n" + "5)Back" + "\n" + "Select: ");
+    }
+    public static void mazdab(){
+        System.out.println("Mazda Biante 2.0 SkyActiv-G (2014)");
+        System.out.println("Engine Tech: 16-valve DOHC, direct fuel injection, naturally-aspirated with variable valve timing");
+        System.out.println("Capacity: 1998 cc");
+        System.out.println("Power: 149 hp at 6,000 rpm");
+        System.out.println("Bore x Stroke: 83.5mm x 91.2mm");
+    }
+    public static void mazdacx9(){
+        System.out.println("Mazda CX-9 2.5 SkyActiv-G Turbo 2WD (2020)");
+        System.out.println("Engine Tech: 16-valve DOHC, naturally-aspirated, turbocharged");
+        System.out.println("Capacity: 2488 cc");
+        System.out.println("Power: 228 hp at 5,000rpm");
+        System.out.println("Bore x Stroke: 89mm x 100mm");
+    }
+    public static void mazdabt(){
+        System.out.println("Mazda BT-50 2.2 MT (2018)");
+        System.out.println("Engine Tech: 16-valve DOHC, naturally-aspirated, common-rail, turbocharged");
+        System.out.println("Capacity: 2198 cc");
+        System.out.println("Power: 150 hp at 3,700 rpm");
+        System.out.println("Bore x Stroke: 86mm x 94.6mm");
+    }
+    public static void mazdacx30(){
+        System.out.println("Mazda CX-30 2.0 G SkyActiv-G 2WD (2020)");
+        System.out.println("Engine Tech: 16-valve DOHC, Naturally Aspirated with Direct Injection and Variable Valve Timing");
+        System.out.println("Capacity: 1998 cc");
+        System.out.println("Power: 162 hp at 6,000 rpm");
+        System.out.println("Bore x Stroke: 83.5mm x 91.2mm");
+    }
+}
