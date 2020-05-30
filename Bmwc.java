af8f72b9fd30e4b12c4e2333430e455f49840a90
Cheang Jia Jun <61217047+jiajunc09@users.noreply.github.com>
2020-05-30 11:35:04 +0800

    Create Bmwc.java

---
+++ Bmwc.java
@@ -0,0 +1,33 @@
+public class Bmwc extends Car{
+    public static void bmw(){
+        System.out.println("Select Car to Review The Specification" + "\n" + "1)BMW 740Le" + "\n" + "2)BMW M850i" + "\n" + "3)BMW X7" + "\n" + "4)BMW Z4" + "\n" + "5)Back" + "\n" + "Select: ");
+    }
+    public static void bmw740(){
+        System.out.println("BMW 740Le xDrive Design Pure Excellence (2019)");
+        System.out.println("Engine Tech: 24-valve DOHC direct injection with twin-scroll turbocharger, variable valve timing");
+        System.out.println("Capacity: 2998 cc");
+        System.out.println("Power: 282 hp at 5,000 rpm");
+        System.out.println("Bore x Stroke: 82mm x 94.6mm");
+    }
+    public static void bmwm850i(){
+        System.out.println("BMW M850i xDrive Coupe (2019)");
+        System.out.println("Engine Tech: 32-valve DOHC, direct fuel injection, twin-turbo with variable valve timing");
+        System.out.println("Capacity: 4395 cc");
+        System.out.println("Power: 530 hp at 5,500 rpm");
+        System.out.println("Bore x Stroke: 88.3mm x 89mm");
+    }
+    public static void bmwx7(){
+        System.out.println("BMW X7 xDrive40i Design Pure Excellence (2019)");
+        System.out.println("Engine Tech: 24-valve DOHC direct injection with twin-scroll turbocharger, variable valve timing");
+        System.out.println("Capacity: 2998 cc");
+        System.out.println("Power: 335 hp at 5,500 rpm ");
+        System.out.println("Bore x Stroke: 82mm x 94.6mm");
+    }
+    public static void bmwz4(){
+        System.out.println("BMW Z4 sDrive30i M Sport (2019)");
+        System.out.println("Engine Tech: 16-valve DOHC, direct fuel injection, twin-scroll turbo with variable valve timing");
+        System.out.println("Capacity: 1998 cc");
+        System.out.println("Power: 255 hp at 5,000 rpm");
+        System.out.println("Bore x Stroke: 82mm x 94.6mm");
+    }
+}
